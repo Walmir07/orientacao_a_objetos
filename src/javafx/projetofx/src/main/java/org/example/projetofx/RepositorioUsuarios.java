3846cc2e8cd77390902729c4a83e736f64ec838f
Walmir07 <walmirlima.dev@gmail.com>
2026-05-25 13:56:22 -0300

    Adicionando Optional no Repositorio de usuários para lidar com a autenticação

--- src/javafx/projetofx/src/main/java/org/example/projetofx/RepositorioUsuarios.java
+++ src/javafx/projetofx/src/main/java/org/example/projetofx/RepositorioUsuarios.java
@@ -8,11 +8,10 @@ import java.util.Optional;
 public class RepositorioUsuarios {
 
     // Padrão de projeto singleton --> Centralizar dados de instância.
-
     private static final RepositorioUsuarios INSTANCIA = new RepositorioUsuarios();
 
     private RepositorioUsuarios() {
-        if(usuarios == null) {
+        if (usuarios == null) {
             usuarios = new HashMap<>();
             criarUsuariosExemplo();
         }
@@ -25,23 +24,23 @@ public class RepositorioUsuarios {
     private Map<String, Usuario> usuarios;
 
     private void criarUsuariosExemplo() {
-        usuarios.put("antonio", new Usuario("antonio", "123"));
-        usuarios.put("joao", new Usuario("joao", "456"));
+        Usuario u1 = new Usuario("antonio", "123");
+        u1.getProdutos().add(new Produto("Teclado", 45.99));
+        u1.getProdutos().add(new Produto("Webcam", 145.99));
+        usuarios.put("antonio", u1);
+
+        Usuario u2 = new Usuario("joao", "234");
+        u2.getProdutos().add(new Produto("Monitor", 2345.99));
+        u2.getProdutos().add(new Produto("Mouse", 235.99));
+        usuarios.put("joao", u2);
     }
 
-    public Optional<Usuario> autenticar(String nome, String senha){
-
-        Usuario u1 = new Usuario("antonio", "123");
-        u1.getProdutos().add(new Produto("Teclado", 100.0));
-
+    public Optional<Usuario> autenticar(String nome, String senha) {
         Usuario u = usuarios.get(nome);
-
-        if(u != null && u.getSenha().equals(senha)){
+        if (u != null && u.getSenha().equals(senha)) {
             return Optional.of(u);
         }
 
         return Optional.empty();
-
     }
-
 }
